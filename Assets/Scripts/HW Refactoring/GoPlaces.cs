94bfeb6508ec494c55c97994489248b8c87ca470
Zhugelyn <kibitkin_00@list.ru>
2023-11-15 21:14:27 +0400

    HW Гений Code Style

---
+++ Assets/Scripts/HW Refactoring/GoPlaces.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class GoPlaces : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Transform _placesPoint;
+    private Transform[] _places;
+    private int _currentPlaceIndex;
+
+    private void Start()
+    {
+        _places = new Transform[_placesPoint.childCount];
+
+        for (int i = 0; i < _placesPoint.childCount; i++)
+        {
+            _places[i] = _placesPoint.GetChild(i).GetComponent<Transform>();
+        }
+    }
+
+    private void Update()
+    {
+        var _place = _places[_currentPlaceIndex];
+        transform.position = Vector3.MoveTowards(transform.position, _place.position, _speed * Time.deltaTime);
+
+        if (transform.position == _place.position)
+        {
+            UpdateNextPlaceLogic();
+        }
+    }
+
+    private void UpdateNextPlaceLogic()
+    {
+        _currentPlaceIndex++;
+
+        if (_currentPlaceIndex == _places.Length)
+        {
+            _currentPlaceIndex = 0;
+        }
+    }
+}
